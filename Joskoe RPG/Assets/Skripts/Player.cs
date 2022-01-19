75ac5248b56dc205d4370641c66c834e5ebe51e7
Neon-Armorina <78750945+Neon-Armorina@users.noreply.github.com>
2022-01-19 16:56:54 +0300

    Moving And Manual Collision Detection

--- Joskoe RPG/Assets/Skripts/Player.cs
+++ Joskoe RPG/Assets/Skripts/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
     // Some variables
     private BoxCollider2D _boxCollider;
     private Vector3 _moveDelta;
+    private RaycastHit2D hit;
 
     // Start is called before the first frame update
     private void Start(){
@@ -29,7 +30,18 @@ public class Player : MonoBehaviour
         else if (_moveDelta.x < 0)
             transform.localScale = new Vector3(-1, 1, 1);
 
-        // Player movement
-        transform.Translate(_moveDelta * Time.deltaTime);
+        // Check we can move in this direction, by casting a box there first, if the box returns null, we're free to move
+        hit = Physics2D.BoxCast(transform.position, _boxCollider.size, 0, new Vector2(0, _moveDelta.y), Mathf.Abs(_moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking")); 
+        if (hit.collider == null){
+            // Player movement
+            transform.Translate(0, _moveDelta.y * Time.deltaTime, 0);
+        }
+
+        hit = Physics2D.BoxCast(transform.position, _boxCollider.size, 0, new Vector2(_moveDelta.x, 0), Mathf.Abs(_moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (hit.collider == null)
+        {
+            // Player movement
+            transform.Translate(_moveDelta.x * Time.deltaTime, 0, 0);
+        }
     }
 }
